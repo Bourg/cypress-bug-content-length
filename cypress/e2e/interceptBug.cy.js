04ccb98117c46059bd73fd5245a8e5edc0a4f62f
Austin Bourgerie <abourgerie@foundationmedicine.com>
2022-10-26 10:12:22 -0400

    Cleanup

--- cypress/e2e/interceptBug.cy.js
+++ cypress/e2e/interceptBug.cy.js
@@ -1,24 +1,25 @@
-describe("Intercept Bug", () => {
+describe("cy.intercept should not change headers", () => {
   it("receives no content-length header without cy.intercept", () => {
-    cy.visit("localhost:3000");
+    cy.visit("");
 
     cy.contains("No Content-Length header").should("be.visible");
   });
 
-  it("receives a content-length header when cy.intercept is used to spy", () => {
+  it("receives no content-length header when cy.intercept is used to spy", () => {
     // Using cy.intercept to spy is what causes this bug!
     // This test case is identical to the previous one, except it uses cy.intercept to spy
     cy.intercept("/api");
 
-    cy.visit("localhost:3000");
+    cy.visit("");
 
+    // This will fail the test because the server did receive a Content-Length header!
     cy.contains("No Content-Length header").should("be.visible");
   });
 
   it("receives no content-length header when cy.intercept is used to stub", () => {
     cy.intercept("/api", { a: 1, b: 2, c: 3 });
 
-    cy.visit("localhost:3000");
+    cy.visit("");
 
     cy.contains("No Content-Length header").should("be.visible");
   });
